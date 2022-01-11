4388d941a48ea8d873634e3dfdc18bcafa5bb9f8
lolaPandaERP <86048238+lolaPandaERP@users.noreply.github.com>
2022-01-11 16:31:48 +0100

    module 1.0

---
+++ class/actions_alertpop.class.php
@@ -0,0 +1,441 @@
+<?php
+/* Copyright (C) 2022 SuperAdmin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    alertpop/class/actions_alertpop.class.php
+ * \ingroup alertpop
+ * \brief   Example hook overload.
+ *
+ * Put detailed description here.
+ */
+
+/**
+ * Class ActionsAlertPop
+ */
+class ActionsAlertPop
+{
+	/**
+	 * @var DoliDB Database handler.
+	 */
+	public $db;
+
+	/**
+	 * @var string Error code (or message)
+	 */
+	public $error = '';
+
+	/**
+	 * @var array Errors
+	 */
+	public $errors = array();
+
+
+	/**
+	 * @var array Hook results. Propagated to $hookmanager->resArray for later reuse
+	 */
+	public $results = array();
+
+	/**
+	 * @var string String displayed by executeHook() immediately after return
+	 */
+	public $resprints;
+
+
+	/**
+	 * Constructor
+	 *
+	 *  @param		DoliDB		$db      Database handler
+	 */
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array			$parameters		Array of parameters
+	 * @param	CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param	string			$action      	'add', 'update', 'view'
+	 * @return	int         					<0 if KO,
+	 *                           				=0 if OK but we want to process standard actions too,
+	 *                            				>0 if OK and we want to replace standard actions.
+	 */
+	public function getNomUrl($parameters, &$object, &$action)
+	{
+		global $db, $langs, $conf, $user;
+		$this->resprints = '';
+		return 0;
+	}
+
+	/**
+	 * Overloading the doActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function doActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))	    // do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			// Do what you want here...
+			// You can for example call global vars like $fieldstosearchall to overwrite them, or update database depending on $action and $_POST values.
+		}
+
+		if (!$error) {
+			$this->results = array('myreturn' => 999);
+			$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+	/**
+	 * Overloading the doMassActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function doMassActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			foreach ($parameters['toselect'] as $objectid)
+			{
+				// Do action on each object id
+			}
+		}
+
+		if (!$error) {
+			$this->results = array('myreturn' => 999);
+			$this->resprints = 'A text to show';
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+	/**
+	 * Overloading the addMoreMassActions function : replacing the parent's function with the one below
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   CommonObject    $object         The object to process (an invoice if you are in invoice module, a propale in propale's module, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function addMoreMassActions($parameters, &$object, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$error = 0; // Error counter
+		$disabled = 1;
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+			$this->resprints = '<option value="0"'.($disabled ? ' disabled="disabled"' : '').'>'.$langs->trans("AlertPopMassAction").'</option>';
+		}
+
+		if (!$error) {
+			return 0; // or return 1 to replace standard code
+		} else {
+			$this->errors[] = 'Error message';
+			return -1;
+		}
+	}
+
+
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array	$parameters     Array of parameters
+	 * @param   Object	$object		   	Object output on PDF
+	 * @param   string	$action     	'add', 'update', 'view'
+	 * @return  int 		        	<0 if KO,
+	 *                          		=0 if OK but we want to process standard actions too,
+	 *  	                            >0 if OK and we want to replace standard actions.
+	 */
+	public function beforePDFCreation($parameters, &$object, &$action)
+	{
+		global $conf, $user, $langs;
+		global $hookmanager;
+
+		$outputlangs = $langs;
+
+		$ret = 0; $deltemp = array();
+		dol_syslog(get_class($this).'::executeHooks action='.$action);
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2')))		// do something only for the context 'somecontext1' or 'somecontext2'
+		{
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * Execute action
+	 *
+	 * @param	array	$parameters     Array of parameters
+	 * @param   Object	$pdfhandler     PDF builder handler
+	 * @param   string	$action         'add', 'update', 'view'
+	 * @return  int 		            <0 if KO,
+	 *                                  =0 if OK but we want to process standard actions too,
+	 *                                  >0 if OK and we want to replace standard actions.
+	 */
+	public function afterPDFCreation($parameters, &$pdfhandler, &$action)
+	{
+		global $conf, $user, $langs;
+		global $hookmanager;
+
+		$outputlangs = $langs;
+
+		$ret = 0; $deltemp = array();
+		dol_syslog(get_class($this).'::executeHooks action='.$action);
+
+		/* print_r($parameters); print_r($object); echo "action: " . $action; */
+		if (in_array($parameters['currentcontext'], array('somecontext1', 'somecontext2'))) {
+			// do something only for the context 'somecontext1' or 'somecontext2'
+		}
+
+		return $ret;
+	}
+
+
+
+	/**
+	 * Overloading the loadDataForCustomReports function : returns data to complete the customreport tool
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int                             < 0 on error, 0 on success, 1 to replace standard code
+	 */
+	public function loadDataForCustomReports($parameters, &$action, $hookmanager)
+	{
+		global $conf, $user, $langs;
+
+		$langs->load("alertpop@alertpop");
+
+		$this->results = array();
+
+		$head = array();
+		$h = 0;
+
+		if ($parameters['tabfamily'] == 'alertpop') {
+			$head[$h][0] = dol_buildpath('/module/index.php', 1);
+			$head[$h][1] = $langs->trans("Home");
+			$head[$h][2] = 'home';
+			$h++;
+
+			$this->results['title'] = $langs->trans("AlertPop");
+			$this->results['picto'] = 'alertpop@alertpop';
+		}
+
+		$head[$h][0] = 'customreports.php?objecttype='.$parameters['objecttype'].(empty($parameters['tabfamily']) ? '' : '&tabfamily='.$parameters['tabfamily']);
+		$head[$h][1] = $langs->trans("CustomReports");
+		$head[$h][2] = 'customreports';
+
+		$this->results['head'] = $head;
+
+		return 1;
+	}
+
+
+
+	/**
+	 * Overloading the restrictedArea function : check permission on an object
+	 *
+	 * @param   array           $parameters     Hook metadatas (context, etc...)
+	 * @param   string          $action         Current action (if set). Generally create or edit or null
+	 * @param   HookManager     $hookmanager    Hook manager propagated to allow calling another hook
+	 * @return  int 		      			  	<0 if KO,
+	 *                          				=0 if OK but we want to process standard actions too,
+	 *  	                            		>0 if OK and we want to replace standard actions.
+	 */
+	public function restrictedArea($parameters, &$action, $hookmanager)
+	{
+		global $user;
+
+		if ($parameters['features'] == 'myobject') {
+			if ($user->rights->alertpop->myobject->read) {
+				$this->results['result'] = 1;
+				return 1;
+			} else {
+				$this->results['result'] = 0;
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+
+	public function addMoreActionsButtons($parameters, &$object, &$action, $hookmanager){
+
+global $db, $conf, $langs;
+
+	if($parameters['currentcontext'] == 'thirdpartycomm'){
+
+		$socid = GETPOST('socid');
+		$societe = new Societe($db);
+		$societe->fetch($socid);
+
+		// renvoie le montant en cours (reste a payer)
+		$tmp = $object->getOutstandingBills('customer', 0);
+		$outstandingOpened = $tmp['opened'];
+
+		?>
+
+		<!DOCTYPE html>
+		<html>
+		<head>
+
+			<!-- Loading librairies jquery for popup alert -->
+			<script type="text/javascript" src="<https://code.jquery.com/jquery-1.8.2.js>"></script>
+		<head>
+
+		<!-- Style CSS for popup -->
+		<style type="text/css">
+		#overlay {
+		position: fixed;
+		top: 100px;
+		/* left: 50%; */
+		width: 100%;
+		height: 100%;
+		display: none;
+		/* align-self: auto; */
+		}
+		.popup{
+		width: 100%;
+		top: 0;
+		right: 50%;
+		left: 50px;
+		display: grid;
+		position: absolute;
+		z-index: 101;
+		}
+		.cnt223{
+		min-width: 100px;
+		width: 400px;
+		min-height: 100px;
+		margin: 100px auto;
+		background: #fff;
+		position: relative;
+		z-index: 103;
+		padding: 15px 35px;
+		border-radius: 5px;
+		box-shadow: 0 3.5px 4px #34495E;
+		}
+
+		.cnt223 p{
+			text-align: center;
+			clear: both;
+			color: #000;
+			text-align: center;
+			font-size: 20px;
+			font-family: sans-serif;
+		}
+		.cnt223 button{
+			border-radius: 0.20em;
+			background: initial;
+			background-color: #C70039;
+			color: #fff;
+			font-size: 1.2em;
+		}
+		.cnt223 h1{
+			color: #d91900;
+			text-align: center;
+			font-size: 25px;
+			font-family: arial,tahoma,verdana,helvetica;
+		}
+		.cnt223 h2{
+			color: #34495E;
+			text-align: center;
+			font-size: 25px;
+			font-family: arial,tahoma,verdana,helvetica;
+		}
+		</style>
+
+		<!-- Script popup -->
+		<script type='text/javascript'>
+		<?php
+		// Condition for opened popup
+		if($outstandingOpened > 0){
+			?>
+			$(function(){
+			var overlay = $('<div id="overlay"></div>');
+			overlay.show();
+			overlay.appendTo(document.body);
+			$('.popup').show();
+			$('.close').click(function(){
+			$('.popup').hide();
+			overlay.appendTo(document.body).remove();
+			return false;
+			});
+
+			$('.x').click(function(){
+			$('.popup').hide();
+			overlay.appendTo(document.body).remove();
+			return false;
+			});
+			});
+		</script>
+		<?php
+		}
+		?>
+
+<!-- TEXTE FOR POPUP -->
+		<div class='popup'>
+			<div class='cnt223'>
+				<h1>ATTENTION</h1>
+				<h2>Client en impayé !</h2>
+				<p>
+				<button class='close'>ok</button>
+				</br>
+				</p>
+			</div>
+		</div>
+	</html>
+		<?php
+	}
+ }
+}
+
